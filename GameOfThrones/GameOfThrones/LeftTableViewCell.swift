a99e53d981adb578fb75bb20fe127a31a1ddd530
Adán Diaz <adamdiaz@pursuit.org>
2019-11-20 14:30:31 -0500

    added tableviewcell files for cells

---
+++ GameOfThrones/GameOfThrones/LeftTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  LeftTableViewCell.swift
+//  GameOfThrones
+//
+//  Created by Adam Diaz on 11/20/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class LeftTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var episodeImage: UIImageView!
+    
+    @IBOutlet weak var episodeNameLabel: UILabel!
+    
+    @IBOutlet weak var seasonNum: UILabel!
+    
+    @IBOutlet weak var episodeNum: UILabel!
+
+}
---
+++ GameOfThrones/GameOfThrones/RightTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  RightTableViewCell.swift
+//  GameOfThrones
+//
+//  Created by Adam Diaz on 11/20/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class RightTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var episodeImage: UIImageView!
+    
+    @IBOutlet weak var episodeNameLabel: UILabel!
+    
+    @IBOutlet weak var seasonNum: UILabel!
+    
+    @IBOutlet weak var episodeNum: UILabel!
+
+}
